a044086938f2636b86e84c92df73cae855bf5efe
Aliya <lyubopytnaya_87@mail.ru>
2020-12-19 17:12:28 +0300

    Lesson2. Сделала 1-5 задания

---
+++ task_1.py
@@ -0,0 +1,25 @@
+"""
+Проверить механизм наследования в Python.
+
+Для этого создать два класса. Первый — родительский (ItemDiscount),
+должен содержать статическую информацию о товаре: название и цену.
+
+Второй — дочерний (ItemDiscountReport),
+должен содержать функцию (get_parent_data), отвечающую
+за отображение информации о товаре в одной строке.
+
+Проверить работу программы.
+"""
+class ItemDiscount:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+class ItemDiscountReport(ItemDiscount):
+    def get_parent_data(self):
+        return f'Наименование товара: {self.name}. Цена: {self.price}'
+
+
+Item_report = ItemDiscountReport("Брюки", 1000)
+print(Item_report.get_parent_data())
---
+++ task_2.py
@@ -0,0 +1,18 @@
+"""
+2. Инкапсулировать оба параметра (название и цену)
+товара родительского класса.
+Убедиться, что при сохранении текущей логики работы программы
+будет сгенерирована ошибка выполнения.
+"""
+class ItemDiscount:
+    def __init__(self, name, price):
+        self.__name = name
+        self.__price = price
+
+
+class ItemDiscountReport(ItemDiscount):
+    def get_parent_data(self):
+        return f'Наименование товара: {self.name}. Цена: {self.price}'
+
+Item = ItemDiscountReport("Брюки", 1000)
+print(Item.get_parent_data())
